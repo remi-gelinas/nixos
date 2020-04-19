fd1bdfac2dfac5fc95a24d61cde9eded21e43793
Remi Gelinas <mail@remigelin.as>
2020-04-18 23:47:42 -0400

    fix(overlays/st): fix patch sources and add harfbuzz

--- overlays/st.nix
+++ overlays/st.nix
@@ -1,24 +1,19 @@
 final: prev: {
   st = let
     # Alpha patch
-    alpha = prev.fetchPatch {
+    alpha = prev.fetchpatch {
       url = "https://st.suckless.org/patches/alpha/st-alpha-0.8.2.diff";
       sha256 = "158k93bbgrmcajfxvkrzfl65lmqgj6rk6kn8yl6nwk183hhf5qd4";
     };
 
     # Ligature patch with alpha patch compatibility
-    ligatures-alpha = prev.fetchPatch {
-      url = "https://github.com/cog1to/st-ligatures/raw/master/st-ligatures-alpha-scrollback-20200406-28ad288.diff";
-      sha256 = "1lvvlx2xh6dgr363hg9hfd6nr87nqksrlhkfjdaz6k86ahcn7srf";
-    };
-
-    # Nord theme patch
-    nord = prev.fetchpatch {
-      url = "https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff";
-      sha256 = "03b709p3mywydzsi5gx8j24krb63kzm5if4dyqq7s0ahh85qiv7n";
+    ligatures-alpha = prev.fetchpatch {
+      url = "https://github.com/cog1to/st-ligatures/raw/master/st-ligatures-alpha-20200406-28ad288.diff";
+      sha256 = "0n7irfnhh686qwjdm71fk0c5x1qjklaflss34jpvcr7pjcvs2php";
     };
   in
     prev.st.override {
-      patches = [ alpha, ligatures-alpha, nord ];
+      patches = [ alpha ligatures-alpha ];
+      extraLibs = [ prev.harfbuzz ];
     };
 }
