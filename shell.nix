157806534e22f9827a87f5e82131d86d9746a7dd
remi-gelinas <mail@remigelin.as>
2020-06-05 22:16:55 -0400

    refactor: simplify shell

--- shell.nix
+++ shell.nix
@@ -1,23 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  configs = "${toString ./.}#nixosConfigurations";
-  hostname = pkgs.lib.fileContents /etc/hostname;
-  build = "config.system.build";
-
-  rebuild = pkgs.writeShellScriptBin "rebuild" ''
-    if [[ -z $1 ]]; then
-      echo "Usage: $0 [host] {switch|boot|test|iso}"
-    elif [[ $1 == "iso" ]]; then
-      nix build ${configs}.niximg.${build}.isoImage
-    elif [[ -z $2 ]]; then
-      sudo -E nix run -vv ${configs}.${hostname}.${build}.toplevel -c switch-to-configuration $1
-    else
-      sudo -E nix run -vv ${configs}.$1.${build}.toplevel -c switch-to-configuration $2
-    fi
-  '';
+  flakes = import "${pkgs.path}/nixos" { configuration.nix.package = pkgs.nixFlakes; };
 in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ git git-crypt nixFlakes rebuild ];
+  nativeBuildInputs = with pkgs; [ git git-crypt ];
+  buildInputs = with flakes; with config.system.build; [ nixos-rebuild ];
 
   NIX_CONF_DIR = let
     current = pkgs.lib.optionalString (builtins.pathExists /etc/nix/nix.conf)
